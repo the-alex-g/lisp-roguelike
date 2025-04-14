ee1771c8e8a665b82cd9990f81f9f1d5371eec80
Alex G <alex.gestwicki@gmail.com>
2025-04-14 07:52:43 -0400

    Fix quaffing

--- codex.lisp
+++ codex.lisp
@@ -1,3 +1,5 @@
+;; EQUIPMENT
+
 (defequipment potion ())
 (add-to-shop
  (defequipment sword () :atk '(1 6 0 0 slashing) :weaponp t)
@@ -13,6 +15,8 @@
    ((healing (roll 2 4))) :inherit potion))
 (defequipment kobold-meat ()
   :sustenance (roll 1 10 5) :inherit food :description "recovers 6-15 hunger")
+(defequipment goblin-meat ()
+  :sustenance (roll 2 10 10) :inherit food :description "recovers 21-13 hunger")
 (defequipment fist () :atk '(1 3 -1 0 bludgeoning) :weaponp t)
 (defequipment gold (amount) :solidp nil :color 'yellow :char #\*)
 
@@ -20,6 +24,8 @@
   (let ((gold (make-instance 'gold)))
     (setf (amount gold) amount)
     gold))
+
+;; ACTORS
 
 (defactor corpse #\c (loot) :solidp nil)
 (defactor ladder #\# (direction) :solidp nil)
@@ -31,8 +37,10 @@
 (defactor pit-trap #\! () :inherit trap :solidp nil)
 (defactor shopkeeper-pedestal #\I () :solidp nil)
 
+;; ENEMIES
+
 (defenemy goblin #\g () :color 32 :health (roll 1 4 1) :equips (make-dagger) :dex 1
-  :meat (roll 2 10 10)
+  :meat (make-goblin-meat)
   :loot (list (make-gold (roll 1 6)) (make-dagger)))
 (defenemy goblin-archer #\g () :inherit goblin
   :equips (make-bow)
@@ -43,6 +51,8 @@
   :str 2 :dex -1 :armor 1 :spd 3/4 :con 2 :meat (roll 2 10 20))
 (defenemy shopkeeper #\U ((domain 4) (enragedp nil)) :color 'purple :health (roll 3 10 5)
   :equips (list (make-sword-+1) (make-sword-+1)))
+
+;; STATUSES
 
 (defstatus resting :duration -1 :speed 0.2)
 (defstatus frightened)
